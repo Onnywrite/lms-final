ee4c11e5373442a1dc9bd3c11edefe8e0b27c2a8
Onnywrite <126114615+Onnywrite@users.noreply.github.com>
2024-02-12 00:58:27 +0300

    Link json name with field name

--- internal/domain/models/types.go
+++ internal/domain/models/types.go
@@ -4,7 +4,7 @@ type Expression struct {
 	Expression      string `json:"expression"`
 	AdditionTime    int    `json:"addition_time"`
 	SubtractionTime int    `json:"subtraction_time"`
-	MultiplyingTime int    `json:"multiplication_time"`
+	MultiplicationTime int    `json:"multiplication_time"`
 	DivisionTime    int    `json:"division_time"`
 }
 
