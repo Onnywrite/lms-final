3cbb3356a484a3db49c2e722a76da122ade077ec
Onny Write <vlad.mokeev.061611@gmail.com>
2024-02-13 21:36:11 +0300

    added deamon
    empty deamon files
    implemented daemon_config

---
+++ cmd/daemon/daemon.go
@@ -0,0 +1,18 @@
+package daemon
+
+func main() {
+	/*
+		cfg := config.MustLoadDaemon()
+
+		application := app.NewDaemon()
+		go application.MustStart()
+
+		// graceful shutdown
+		shutdown := make(chan os.Signal, 1)
+		signal.Notify(shutdown, syscall.SIGTERM, syscall.SIGINT)
+
+		<-shutdown
+
+		application.Stop()
+	*/
+}
